1fdfb44690feb57c94dc4ab6a21a185ca4ebad13
Josselin CHEVALAY <josselin.chevalay@riskdata.com>
2016-09-30 15:45:28 +0200

    implement docker network

--- terraform/my_first_arch.tf
+++ terraform/my_first_arch.tf
@@ -13,11 +13,16 @@ resource "docker_image" "tuto-lb" {
 resource "docker_network" "network" {
     name   = "network"
     driver = "bridge"
+    ipam_config {
+        subnet = "172.17.0.1/16"
+        gateway = "172.17.0.1"
+    }
 }
 
 resource "docker_container" "app1" {
    image = "${docker_image.tuto-node.latest}"
    name  = "app1"
+   networks = ["${docker_network.network.name}"]
    ports {
        internal = "${var.app-port}"
        external = 3000
@@ -27,6 +32,7 @@ resource "docker_container" "app1" {
 resource "docker_container" "app2" {
    image = "${docker_image.tuto-node.latest}"
    name = "app2"
+   networks = ["${docker_network.network.name}"]
    ports {
      internal = "${var.app-port}"
      external = 3001
@@ -36,6 +42,7 @@ resource "docker_container" "app2" {
 resource "docker_container" "lb" {
    image ="${docker_image.tuto-lb.latest}"
    name  = "lb"
+   networks = ["${docker_network.network.name}"]
    ports {
       internal = 80
       external = 80
